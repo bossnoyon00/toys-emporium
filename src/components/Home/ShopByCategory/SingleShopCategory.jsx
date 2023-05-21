13a79a1e90837030a1d422177771804cf2246ee9
bossnoyon00 <bossnoyon584@gmail.com>
2023-05-21 15:22:33 +0600

    single shop input data

--- src/components/Home/ShopByCategory/SingleShopCategory.jsx
+++ src/components/Home/ShopByCategory/SingleShopCategory.jsx
@@ -5,14 +5,7 @@ const SingleShopCategory = ({ toy }) => {
     return (
         <div className="card w-96 bg-base-100 shadow-xl image-full">
             <figure><img src={img} alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">{subCategory}</h2>
-                <p>{price}</p>
-                <p>{ratings}</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">View Details</button>
-                </div>
-            </div>
+           
         </div>
     );
 };
